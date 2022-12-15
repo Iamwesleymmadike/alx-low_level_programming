3f6bf5bc7ab71b4441ff145f0ebf20389ae68eab
Iamwesleymmadike <wesleymmadike16@gmail.com>
2022-12-15 23:54:40 +0100

    task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isuper-uppercase letters
+ *@c: char check
+ *Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+  if(c >= 'A' && c <= 'Z')
+    return (1);
+      
+    else
+      return (0);
+}
